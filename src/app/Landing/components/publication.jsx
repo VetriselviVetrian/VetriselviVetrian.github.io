74733e161a50877ea3ed46dee1fb78f2a1885d09
arunamaithi <arunamaithirajan@gmail.com>
2025-01-07 10:56:59 +0000

    updated

--- src/app/Landing/components/publication.jsx
+++ src/app/Landing/components/publication.jsx
@@ -102,37 +102,36 @@ export default function Publications() {
   return (
     <div id="publications" className="min-h-screen w-full bg-base-200 pt-20">
       <div className="container mx-auto px-6 lg:px-8 py-12 md:py-16">
-        <div className="flex flex-col md:flex-row md:space-x-16">
-          {/* Left Column */}
-          <div className="md:w-1/4 flex flex-col items-center md:items-start mb-8 md:mb-0 lg:ml-32">
-            <h1 className="text-5xl font-medium mb-4 mr-5">Publications</h1>
-            <p className="text-xl font-medium leading-relaxed ml-6 sm:ml-0">
-              Scholarly publications & pre-prints
-            </p>
-          </div>
+        <div className="text-center mb-12">
+          <h1 className="text-5xl font-bold text-center mb-4">
+            Selected Publications
+          </h1>
+          <p className="text-xl font-medium leading-relaxed">
+            A curated list of publications
+          </p>
+        </div>
 
-          {/* Right Column - Citations */}
-          <div className="md:w-3/4">
-            <div className="space-y-12">
-              {groupedCitations.map(({ year, citations }) => (
-                <div key={year} className="card bg-base-100 shadow-md p-8 lg:mx-20">
-                  <h2 className="text-4xl font-medium mb-6 tracking-tight">
-                    {year}
-                  </h2>
-                  <div className="space-y-8">
-                    {citations.map((citation, index) => (
-                      <div
-                        key={index}
-                        className="border-l-4 border-gray-200 pl-6 text-lg leading-relaxed font-medium text-justify"
-                      >
-                        {citation}
-                      </div>
-                    ))}
+        <div className="space-y-12">
+          {groupedCitations.map(({ year, citations }) => (
+            <div
+              key={year}
+              className="card bg-base-100 shadow-md p-8 mx-auto max-w-4xl"
+            >
+              <h2 className="text-4xl font-medium mb-6 tracking-tight text-left">
+                {year}
+              </h2>
+              <div className="space-y-8">
+                {citations.map((citation, index) => (
+                  <div
+                    key={index}
+                    className="border-l-4 border-gray-200 pl-6 text-lg leading-relaxed font-medium text-justify"
+                  >
+                    {citation}
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
